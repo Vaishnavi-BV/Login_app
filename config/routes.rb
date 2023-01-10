6ae18ed60b49789a6fbdb80a3118f3e2d362ae10
Vaishnavi-BV <psvaishnavi2001@gmail.com>
2023-01-10 12:32:50 +0530

    Adding Login Page Using Devise Gem

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
+  devise_for :users
   resources :homes
-  root 'homes#index'
+  root to: "homes#index"
+
+  # as :user do
+  #   get "signin", to: "devise/sessions#new"
+  #   delete "signout",to: "devise/sessions#destroy"
+  #   get "signup", to: "devise/registrations#new"
+  # end
 
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 

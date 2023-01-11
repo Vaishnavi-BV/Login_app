8ba9a375370ba49970e4bb89f2aee150ea2bd9be
Vaishnavi-BV <psvaishnavi2001@gmail.com>
2023-01-11 18:33:57 +0530

    Add the Main Page and Design the page

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,6 @@
 Rails.application.routes.draw do
-  get 'profiles/new'
-  get 'profiles/edit'
-  get 'profiles/show'
   devise_for :users
-  root to: "homes#main"
+  root "homes#index"
   resources :profiles
   
   resources :homes 
